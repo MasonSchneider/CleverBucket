6e0dd64a816bd2756568fc0be415b5f1eb7e9778
Mason Schneier <mason@schneiderscode.com>
2014-10-25 10:41:50 -0700

    Added changing the titles on selection

--- static/js/cleverbucket.js
+++ static/js/cleverbucket.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
 	if ($(".ideaTab").length > 0) {
 		selectedIdea = $(".ideaTab")[0].attributes['id'].value;
 		$("#"+selectedIdea).addClass("selectedIdea");
+
+		$('#idTitle').text($("#"+selectedIdea).children()[0].innerHTML);
+
 		if ($("."+selectedIdea).length > 0) {
 			selectedFeature = $("."+selectedIdea)[0].attributes['id'].value;
 			$("#"+selectedFeature).addClass("selectedFeature");
+			$('#featTitle').text($("#"+selectedFeature).children()[0].innerHTML);
 		}
 	}
 });
@@ -21,18 +25,22 @@ $(".ideaTab").click(function() {
 		$(this).addClass("hidden");
 	});
 
+	$('#idTitle').text($(this).children()[0].innerHTML);
+
 	selectedIdea = $(this).attr('id');
 	$("."+selectedIdea).each(function(index) {
 		if (index == 0) {
 			$(this).addClass("selectedFeature");
 			$("#"+selectedFeature).removeClass("selectedFeature");			
 			selectedFeature = $(this).attr('id');
+			$('#featTitle').text($("#"+selectedFeature).children()[0].innerHTML);
 		}
 		$(this).removeClass("hidden");
 	});
 	if ($("."+selectedIdea).length == 0) {
 		$("#"+selectedFeature).removeClass("selectedFeature");
 		selectedFeature = 'zzzaaaazzzzdfasdfz';
+		$('#featTitle').text("No Features");
 	}
 });
 
@@ -40,4 +48,6 @@ $(".featureTab").click(function() {
 	$("#"+selectedFeature).removeClass("selectedFeature");
 	$(this).addClass("selectedFeature");
 	selectedFeature = $(this).attr('id');
+
+	$('#featTitle').text($(this).children()[0].innerHTML);
 });
