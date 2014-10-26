2b4e31a619bb7661542972f867e8662006ca5611
Mason Schneier <mason@schneiderscode.com>
2014-10-25 17:43:44 -0700

    Added some mobile compatability

--- static/js/cleverbucket.js
+++ static/js/cleverbucket.js
@@ -2,33 +2,56 @@
 var selectedIdea = "";
 var selectedFeature = "";
 
-$(document).ready(function() {
-	if ($(".ideaTab").length > 0) {
-		selectedIdea = $(".ideaTab")[0].attributes['id'].value;
-		$("#"+selectedIdea).addClass("selectedIdea");
+var start = function() {
+	
+	// Initialize for error avoidance
+	selectedIdea = $(".ideaTab")[0].attributes['id'].value;
+	if ($("."+selectedIdea).length > 0) 
+		selectedFeature = $("."+selectedIdea)[0].attributes['id'].value;
 
-		$('#idTitle').text($("#"+selectedIdea).children()[0].innerHTML);
+	// Set height of idea scroll area
+	var taken = $('.profileArea').height();
+	taken += $('.createAndSearch').height();
+	$('.ideaScrollArea').height($(window).height()-taken - 35);
 
-		if ($("."+selectedIdea).length > 0) {
-			selectedFeature = $("."+selectedIdea)[0].attributes['id'].value;
-			$("#"+selectedFeature).addClass("selectedFeature");
-			$('#featTitle').text($("#"+selectedFeature).children()[0].innerHTML);
-			$('#detailArea').val(detailText[selectedIdea+'+'+selectedFeature]);
-			$('#links').empty();
-			for(var link in detailLinks[selectedIdea+'+'+selectedFeature]) {
-				$('#links').append('<li><a href="'+detailLinks[selectedIdea+'+'+selectedFeature][link][1]+'">'+detailLinks[selectedIdea+'+'+selectedFeature][link][0]+'</a></li>');
+	// Prevent auto select on mobile
+	if ($(window).width() < 980) {
+  		$("#"+selectedIdea).removeClass("selectedIdea");
+  		$("#"+selectedFeature).removeClass("selectedFeature");
+	} else {
+		if ($(".ideaTab").length > 0) {
+			$("#"+selectedIdea).addClass("selectedIdea");
+
+			$('#idTitle').text($("#"+selectedIdea).children()[0].innerHTML);
+
+			if ($("."+selectedIdea).length > 0) {
+				selectedFeature = $("."+selectedIdea)[0].attributes['id'].value;
+				$("#"+selectedFeature).addClass("selectedFeature");
+				$('#featTitle').text($("#"+selectedFeature).children()[0].innerHTML);
+				$('#detailArea').val(detailText[selectedIdea+'+'+selectedFeature]);
+				$('#links').empty();
+				for(var link in detailLinks[selectedIdea+'+'+selectedFeature]) {
+					$('#links').append('<li><a href="'+detailLinks[selectedIdea+'+'+selectedFeature][link][1]+'">'+detailLinks[selectedIdea+'+'+selectedFeature][link][0]+'</a></li>');
+				}
+				var canvas = document.getElementById('sketchCanvas');
+				canvas.width = canvas.width;
+				var context = canvas.getContext('2d');
+				var imageObj = new Image();
+				imageObj.onload = function() {
+					context.drawImage(this, 0, 0);
+				};
+				imageObj.src = detailCanvas[selectedIdea+'+'+selectedFeature];
 			}
-			var canvas = document.getElementById('sketchCanvas');
-			canvas.width = canvas.width;
-			var context = canvas.getContext('2d');
-			var imageObj = new Image();
-			imageObj.onload = function() {
-				context.drawImage(this, 0, 0);
-			};
-			imageObj.src = detailCanvas[selectedIdea+'+'+selectedFeature];
 		}
 	}
+}
+
+$( window ).resize(function() {
+	console.log("resize");
+	start();
 });
+
+$(document).ready(function() {start();});
 
 $(".ideaTab").click(function() {
 	$("#"+selectedIdea).removeClass("selectedIdea");
@@ -42,7 +65,7 @@ $(".ideaTab").click(function() {
 
 	selectedIdea = $(this).attr('id');
 	$("."+selectedIdea).each(function(index) {
-		if (index == 0) {			
+		if (index == 0 && $(window).width() > 980) {			
 			$("#"+selectedFeature).removeClass("selectedFeature");
 			$(this).addClass("selectedFeature");			
 			selectedFeature = $(this).attr('id');
@@ -75,6 +98,8 @@ $(".ideaTab").click(function() {
 		var canvas = document.getElementById('sketchCanvas');
 		canvas.width = canvas.width;
 	}
+	$('.ideaCol').addClass('hidden-xs').addClass('hidden-sm');
+	$('.featureCol').removeClass('hidden-xs').removeClass('hidden-sm');
 });
 
 $(".featureTab").click(function() {
@@ -96,4 +121,6 @@ $(".featureTab").click(function() {
 		context.drawImage(this, 0, 0);
 	};
 	imageObj.src = detailCanvas[selectedIdea+'+'+selectedFeature];
+	$('.featureCol').addClass('hidden-xs').addClass('hidden-sm');
+	$('.detailCol').removeClass('hidden-xs').removeClass('hidden-sm');
 });
